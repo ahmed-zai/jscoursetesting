f44560f568d99004b3a2a6121d949fda82195ceb
ahmed-zai <saidahmed0703@gmail.com>
2025-07-14 10:14:13 -0400

    amout

--- calculatArea.js
+++ calculatArea.js
@@ -1,9 +1,11 @@
 let length ;
 let width ;
+let grocery ;
 function calculateArea() {
     length = parseFloat(document.getElementById('length').value);
     width = parseFloat(document.getElementById('width').value);
+    grocery = parseFloat(document.getElementById('grocery').value);
 
-    let area  = length * width  ;
-    document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
+    let area  = length + width + grocery ;
+    document.getElementById('result').innerText = `The total amout is: ${area}`;
 }
