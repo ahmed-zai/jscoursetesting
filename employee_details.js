052c90b1b19fb5d8f0dd9bb3d44da1d9ba3327ff
ahmed-zai <saidahmed0703@gmail.com>
2025-07-17 07:16:31 -0400

    add specializaion

--- employee_details.js
+++ employee_details.js
@@ -50,3 +50,14 @@ function findEmployeeById(employeeId) {
     }
     
 }
+function findBySpecailizaion(specialization) {
+    const foundSp = employees.find(employee => employees.specialization === specialization );
+    if(foundSp){
+        document.getElementById('employeesDetails').innerHTML = `<p>${foundEmployee.id}: ${foundEmployee.name}: ${foundEmployee.name} - ${foundEmployee.department} - $${foundEmployee.salary}</p>`;
+
+    }
+    else {
+        document.getElementById('employeesDetails').innerHTML = 'no employee has been found with this specialization';
+
+    }
+}
