cbc811998a08bb2124417cdd68d1ed72f37f331f
ahmed-zai <saidahmed0703@gmail.com>
2025-07-19 11:28:45 -0400

    debugger

---
+++ DebugCode/debug_code.js
@@ -0,0 +1,27 @@
+function performOperation() {
+    // get user input from input fields
+    let num1 = parseInt(document.getElementById('input1').value);
+    let num2 = parseInt(document.getElementById('input2').value);
+
+    if (!isNaN(num1) && !isNaN(num2)) {
+        let result = multiply(num1, num2);
+
+        displayResult(result);
+
+    }
+    else {
+        displayResult('Please enter valid numbers');
+    }
+}
+function multiply(a ,b) {
+
+    debugger;
+
+    return a * b ;
+
+
+}
+function displayResult(result) {
+    const resultElement = document.getElementById('result');
+    resultElement.textContent = `the result is : ${result}`;
+}
